f8a6b55ff998e8c379044fe868575d35aa7859f6
David Alves de Souza <alves.david@outlook.com>
2020-07-29 20:32:12 -0300

    Add 404 page

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/Home';
 import CategoryRegister from './pages/Register/Category';
 import VideoRegister from './pages/Register/Video';
-
-const Error404 = () => {
-  return (
-    <div style={{ textAlign: 'center', margin: '0 2rem', color: '#efefef'}}>
-      <h1>Página não encontrada! :(</h1>
-      <Link to="/">Voltar para a Home</Link>
-    </div>
-  );
-}
+import Error from './pages/Error';
 
 ReactDOM.render(
   <BrowserRouter>
@@ -21,7 +13,7 @@ ReactDOM.render(
       <Route path="/" exact component={Home} />
       <Route path="/categoria/nova" component={CategoryRegister} />
       <Route path="/video/novo" component={VideoRegister} />
-      <Route component={Error404} />
+      <Route component={Error} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
---
+++ src/pages/Error/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const PageWrapper = styled.div`
+  text-align: center;
+  margin: 0 2rem;
+  color: #efefef;
+
+  img {
+    width: 100%;
+    max-width: 350px;
+  }
+`;
+
+export default function Error() {
+  return (
+    <PageWrapper>
+      <img src="https://media.giphy.com/media/9J7tdYltWyXIY/source.gif" alt="Error 404" />
+      <h1>Página não encontrada! 🙁</h1>
+      <Link to="/">Voltar para a Home</Link>
+    </PageWrapper>
+  );
+}
